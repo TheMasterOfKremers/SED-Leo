85bb326d7b5b150604be8e57194d46afaf6cdfbf
TheMasterOfKremers <148225279+TheMasterOfKremers@users.noreply.github.com>
2024-09-17 21:17:45 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         "dcase_util>=0.2.16",
         "psds_eval>=0.4.0",
         "sed_eval>=0.2.1",
-        "sed_scores_eval==0.0.1",
+        "sed_scores_eval==0.0.2",
         "thop>=0.1.1",
     ],
 )
